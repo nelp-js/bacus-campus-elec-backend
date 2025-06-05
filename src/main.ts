5dd316f61ec19931c3fd3a1b95e1a240aadd3e97
nelp-js <aineltbacus@example.com>
2025-06-05 14:52:26 +0800

    feat: initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT ?? 3001
+  await app.listen(PORT);
+  console.log(`RUNNIN ON PORT : ${PORT}`)
+}
+
+void bootstrap();
