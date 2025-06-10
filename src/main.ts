b33607b929788d513e8b30c8f20a65e8d2ed0eca
nelp <aineltbacus@gmail.com>
2025-06-10 13:18:06 +0800

    feat: added lost content

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const PORT = process.env.PORT ?? 3001
   await app.listen(PORT);
   console.log(`RUNNIN ON PORT : ${PORT}`)
